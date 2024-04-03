632abbdd04ce52c7cc2d1cd8609c1ba29b970d6d
IndexZeroZzz <l0rmin@yandex.com>
2024-04-03 16:18:15 +0300

    Better code

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,17 @@ export default function Home() {
     const [input, setInput] = useState('');
 
     return (
-      <main className="flex min-h-screen flex-col items-center justify-center p-5">
+      <main className="flex min-h-screen flex-col items-center justify-center p-5 text-center">
           <img src={'mouse.png'} height={'200px'} width={'200px'}/>
           <h1 className={'font-bold text-3xl p-5'}>Bad Bats Q&A</h1>
+          <p className={'m-4'}>Тут вы можете задать вопрос нашей команде. Ответы на вопросы будут публиковаться в нашем <a className={'text-blue-500 underline'} href="https://t.me/bad_bats_ftc">Telegram-Канале</a></p>
           <input placeholder={"Ваш вопрос"} value={input}
                  onChange={e => setInput(e.target.value)}
-                 className={'placeholder-neutral-200 text-white transition-all m-5 outline-none p-2 bg-indigo-400 hover:bg-indigo-500 rounded-md'}
+                 className={'placeholder-neutral-200 text-white transition-all m-5 outline-none p-2 bg-indigo-400 hover:bg-indigo-500 rounded-md min-w-[300px]'}
                  type="text"/>
           <a
                  className={'transition-all m-5 outline-none p-2 bg-indigo-400 hover:bg-indigo-500 rounded-md text-white'}
-                 href={`/send/${input}`}
+                 href={`/send/${input.replace(/\//g, "Slash")}`}
           >Отправить <PaperAirplaneIcon className={'inline h-6 ml-1'}/></a>
 
       </main>
--- app/send/[id]/page.tsx
+++ app/send/[id]/page.tsx
@@ -1,13 +1,39 @@
-import {GET} from "@/app/api/send/route";
+
 import {CheckCircleIcon} from "@heroicons/react/24/outline";
+import type {NextRequest} from "next/server";
+import {EmailTemplate} from "@/app/components/email";
+import {Resend} from "resend";
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Bad Bats Q&A",
+    description: "На этой странице вы можете задать вопрос нашей команде.",
+};
 
 export default function Page({ params }: { params: { id: string } }) {
     // @ts-ignore
-    GET(undefined, decodeURI(params.id))
+    email(decodeURIComponent(params.id))
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-5">
+        <main className="flex min-h-screen flex-col items-center justify-center p-5 text-center">
             <CheckCircleIcon className={'m-6 h-20 text-green-600'}/>
             <h1 className={'font-bold text-3xl p-5'}>Ваш вопрос отправлен!</h1>
         </main>
     )
+}
+
+async function email(question: string) {
+    const resend = new Resend(process.env.RESEND);
+    try {
+        // @ts-ignore
+        const data = await resend.emails.send({
+            from: 'Bad Bats Q&A <bb22462@resend.dev>',
+            to: ['i@l0rmin.ru'],
+            subject: 'Новый Вопрос!',
+            react: EmailTemplate({ question: question }),
+        });
+
+        return Response.json(data);
+    } catch (error) {
+        return Response.json({ error });
+    }
 }
